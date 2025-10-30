d3e5e7731f19709f060a9a8820b1fe828c73d4bf
Lucas Leite Lessa <lucasleitelessa@gmail.com>
2025-10-29 21:40:31 -0300

    teste

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -2,7 +2,7 @@ import {dadosempresaType} from './dadosempresaType'
 import {depoimentosType} from './depoimentosType'
 import {metodoType} from './metodoType'
 import {planosTypes} from './planosTypes'
-import {tituloinstitucionalType} from './tituloinstitucionalType'
+
 import {testenivelamentoType} from './testenivelamentoType'
 
-export const schemaTypes = [dadosempresaType, depoimentosType, metodoType, planosTypes, tituloinstitucionalType, testenivelamentoType]
+export const schemaTypes = [dadosempresaType, depoimentosType, metodoType, planosTypes, testenivelamentoType]
